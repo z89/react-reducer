36d732738249edbeb614e3dfa69985d9b53c5e15
z89 <n10234977@qut.edu.au>
2021-12-16 15:45:28 +1000

    clean up

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createContext, useReducer } from 'react';
+
+import Button from './Button';
+
+export const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement'
+}
+
+function reducer(state: any, action: any) {
+  switch (action.type) {
+    case ACTIONS.INCREMENT: 
+    return { counter: state.counter + 1 }
+    case ACTIONS.DECREMENT: 
+    return { counter: state.counter - 1 }
+    default:
+      return state
+    }
+}
+
+interface ICount {
+  counter: number
+}
+  
+interface ICountContext {
+  counter?: ICount
+  countState?: any
+  countDispatch?: any
+}
+
+const initialState: ICount = { 
+  counter: 0 
+};
+  
+export const CountContext = createContext<ICountContext>({counter: initialState})
+
+const [state, dispatch] = useReducer(reducer, initialState)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    return (
+      <CountContext.Provider value={{ countState: state, countDispatch: dispatch}}>
+        <div className="App">
+          <Button/>
+        </div>
+      </CountContext.Provider>
+    );
   </React.StrictMode>,
   document.getElementById('root')
 );
