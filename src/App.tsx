2f090d869feecb04387378452f54047e49e50f86
z89 <n10234977@qut.edu.au>
2022-05-24 22:27:18 +1000

    dumb cleanup commit broke the repo lol

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { createContext, useReducer } from "react";
+import Button from "./Button";
+
+export const ACTIONS = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+};
+
+function reducer(state: any, action: any) {
+  switch (action.type) {
+    case ACTIONS.INCREMENT:
+      return { counter: state.counter + 1 };
+    case ACTIONS.DECREMENT:
+      return { counter: state.counter - 1 };
+    default:
+      return state;
+  }
+}
+interface ICount {
+  counter: number;
+}
+
+interface ICountContext {
+  counter?: ICount;
+  countState?: any;
+  countDispatch?: any;
+}
+
+const initialState: ICount = {
+  counter: 0,
+};
+
+export const CountContext = createContext<ICountContext>({ counter: initialState });
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <CountContext.Provider value={{ countState: state, countDispatch: dispatch }}>
+      <div className="App">
+        <Button />
+      </div>
+    </CountContext.Provider>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,52 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createContext, useReducer } from 'react';
-
-import Button from './Button';
-
-export const ACTIONS = {
-  INCREMENT: 'increment',
-  DECREMENT: 'decrement'
-}
-
-function reducer(state: any, action: any) {
-  switch (action.type) {
-    case ACTIONS.INCREMENT: 
-    return { counter: state.counter + 1 }
-    case ACTIONS.DECREMENT: 
-    return { counter: state.counter - 1 }
-    default:
-      return state
-    }
-}
-
-interface ICount {
-  counter: number
-}
-  
-interface ICountContext {
-  counter?: ICount
-  countState?: any
-  countDispatch?: any
-}
-
-const initialState: ICount = { 
-  counter: 0 
-};
-  
-export const CountContext = createContext<ICountContext>({counter: initialState})
-
-const [state, dispatch] = useReducer(reducer, initialState)
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    return (
-      <CountContext.Provider value={{ countState: state, countDispatch: dispatch}}>
-        <div className="App">
-          <Button/>
-        </div>
-      </CountContext.Provider>
-    );
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
